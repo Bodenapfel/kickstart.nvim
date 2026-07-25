9dbc74447ba5a86a084bcb3dfd693935ccb5d2fa
Bodenapfel <dakorb20@gmail.com>
2026-07-25 23:56:52 +0200

    disable copilot chat

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -1,16 +1,20 @@
-return {
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim', branch = 'master' },
-    },
-    build = 'make tiktoken',
-    opts = {
-      model = 'auto',
-    },
-    keys = {
-      { '<leader>aa', '<cmd>CopilotChatToggle<CR>', desc = 'Toggle Copilot Chat' },
-      { '<leader>aq', '<cmd>CopilotChatQuick<CR>', desc = 'Quick Chat with Copilot' },
-    },
-  },
-}
+-- CopilotChat is disabled because it is not included with the current
+-- GitHub Education Copilot plan. Uncomment the spec below to restore it.
+return {}
+
+-- return {
+--   {
+--     'CopilotC-Nvim/CopilotChat.nvim',
+--     dependencies = {
+--       { 'nvim-lua/plenary.nvim', branch = 'master' },
+--     },
+--     build = 'make tiktoken',
+--     opts = {
+--       -- See Configuration section for options
+--     },
+--     keys = {
+--       { '<leader>aa', '<cmd>CopilotChatToggle<CR>', desc = 'Toggle Copilot Chat' },
+--       { '<leader>aq', '<cmd>CopilotChatQuick<CR>', desc = 'Quick Chat with Copilot' },
+--     },
+--   },
+-- }
