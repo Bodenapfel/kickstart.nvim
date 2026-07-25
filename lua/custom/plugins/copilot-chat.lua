b1fc31187309dc23bd499c4cd27c25d9ee0f55fd
Bodenapfel <dakorb20@gmail.com>
2026-07-25 23:33:47 +0200

    restore copilot setup

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -1,20 +1,16 @@
--- CopilotChat is currently disabled in favor of Avante with the Codex ACP provider.
--- To restore it, remove the `return {}` below and uncomment the plugin spec.
-return {}
-
--- return {
---   {
---     'CopilotC-Nvim/CopilotChat.nvim',
---     dependencies = {
---       { 'nvim-lua/plenary.nvim', branch = 'master' },
---     },
---     build = 'make tiktoken',
---     opts = {
---       -- See Configuration section for options
---     },
---     keys = {
---       { '<leader>aa', '<cmd>CopilotChatToggle<CR>', desc = 'Toggle Copilot Chat' },
---       { '<leader>aq', '<cmd>CopilotChatQuick<CR>', desc = 'Quick Chat with Copilot' },
---     },
---   },
--- }
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      -- See Configuration section for options
+    },
+    keys = {
+      { '<leader>aa', '<cmd>CopilotChatToggle<CR>', desc = 'Toggle Copilot Chat' },
+      { '<leader>aq', '<cmd>CopilotChatQuick<CR>', desc = 'Quick Chat with Copilot' },
+    },
+  },
+}
